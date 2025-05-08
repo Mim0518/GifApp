1091b60467e4825ec3f2e2d75ec8f3cc267ed126
Guillermo Moreno <morenorivera1234@gmail.com>
2025-05-08 11:48:55 -0600

    Update components to use URL input for GIF rendering
    Replaced `Gif` object dependency with direct URL-based inputs across components. This simplifies data flow and improves flexibility by passing URLs directly instead of full objects. Updated necessary templates and logic accordingly.

--- src/app/gifs/components/gift-list/gift-list.component.ts
+++ src/app/gifs/components/gift-list/gift-list.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import type { Gif } from '../../interfaces/gif.interface';
+import {Component, input} from '@angular/core';
 import {GifListItemComponent} from '../gif-list-item/gif-list-item.component';
 
 @Component({
@@ -10,18 +9,5 @@ import {GifListItemComponent} from '../gif-list-item/gif-list-item.component';
   templateUrl: './gift-list.component.html'
 })
 export class GiftListComponent {
-  gifs: Gif[] = [
-    { id: "1", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" },
-    { id: "2", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" },
-    { id: "3", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" },
-    { id: "4", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" },
-    { id: "5", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" },
-    { id: "6", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" },
-    { id: "7", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" },
-    { id: "8", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" },
-    { id: "9", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" },
-    { id: "10", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" },
-    { id: "11", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" },
-    { id: "12", url: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" }
-  ];
+  urls= input.required<string[]>()
 }
