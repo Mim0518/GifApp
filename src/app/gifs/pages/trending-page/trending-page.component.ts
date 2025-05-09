91dfe635e98af91ff545a893f9f5b9368dee3ba1
Guillermo Moreno <morenorivera1234@gmail.com>
2025-05-08 21:40:47 -0600

    Refactor GIF list to use a shared constant
    Replaced inline GIF URLs with a shared `gifs` constant to promote reuse and maintainability. This change simplifies the component by centralizing the URL list in a single initialization.

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import {GiftListComponent} from '../../components/gift-list/gift-list.component';
 
+const gifs: string[] = [
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+];
+
 @Component({
   selector: 'app-trending-page',
   imports: [
@@ -9,18 +24,5 @@ import {GiftListComponent} from '../../components/gift-list/gift-list.component'
   templateUrl: './trending-page.component.html'
 })
 export default class TrendingPageComponent {
-  imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-  ];
+  imageUrls = gifs;
 }
